25def540c4381a15ce91dd9e1a554d4c39d0c64d
student <student@PC633S13.fgakusei.local>
2014-05-26 17:18:24 +0900

    画面遷移

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.hellogalaxy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button btn1 = (Button)findViewById(R.id.button1);
+		Button btn2 = (Button)findViewById(R.id.button2);
+		btn1.setOnClickListener(this);
+		btn2.setOnClickListener(this);
 	}
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+		case R.id.button1:
+			EditText et1 = (EditText)findViewById(R.id.editText1);
+			EditText et2 = (EditText)findViewById(R.id.editText2);
+			TextView tv = (TextView)findViewById(R.id.textView3);
+			tv.setText(et1.getText() + " " + et2.getText());
 
+			Intent vIntent = new Intent(this,MsgActivity.class);
+			startActivity(vIntent);
+			break;
+
+		case R.id.button2:
+			et1 = (EditText)findViewById(R.id.editText1);
+			et2 = (EditText)findViewById(R.id.editText2);
+			tv = (TextView)findViewById(R.id.textView3);
+			et1.setText("");
+			et2.setText("");
+			tv.setText("");
+			break;
+		}
+
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
